7ddd7836ce556d359479dcfe39cb2ee999ae7bb5
Talles Emanuel Moreira Marques <tallesemanuel1@gmail.com>
2023-12-07 16:08:45 -0300

    Modelo sem callback

--- OPC_estudos/Main.cpp
+++ OPC_estudos/Main.cpp
@@ -31,107 +31,34 @@ int main(void)
 	printf("Initializing the COM environment...\n");
 	CoInitialize(NULL);
 
+	/* ---------------------------------------------------------------------------------- */
 	printf("Instantiating the MATRIKON OPC Server for Simulation...\n");
-	pIOPCServer = InstantiateServer(OPC_SERVER_NAME);
-
-	printf("Adding a group in the INACTIVE state for the moment...\n");
-	AddTheGroup(pIOPCServer, pIOPCItemMgt, hServerGroup);
-
-	size_t m;
-	// Saw-toothed Waves
-	wcstombs_s(&m, buf, 100, SAW_ID, _TRUNCATE);
-	printf("Adding the item %s to the group...\n", buf);
-    AddTheItem(pIOPCItemMgt, hServerItemSaw, SAW_ID);
-
-	// Triangle Waves
-	wcstombs_s(&m, buf, 100, TRIANGLE_ID, _TRUNCATE);
-	printf("Adding the item %s to the group...\n", buf);
-    AddTheItem(pIOPCItemMgt, hServerItemTriangle, TRIANGLE_ID);
-
-	// Random
-	wcstombs_s(&m, buf, 100, RANDOM_ID, _TRUNCATE);
-	printf("Adding the item %s to the group...\n", buf);
-    AddTheItem(pIOPCItemMgt, hServerItemRandom, RANDOM_ID);
-
-	VARIANT varValue1, varValue2, varValue3; 
-	VariantInit(&varValue1);
-	VariantInit(&varValue2);
-	VariantInit(&varValue3);
-
-	printf ("Reading synchronously during 10 seconds...\n\n");
-	for (i=0; i<10; i++) {
-		ReadItem(pIOPCItemMgt, hServerItemSaw, varValue1);
-		ReadItem(pIOPCItemMgt, hServerItemTriangle, varValue2);
-		ReadItem(pIOPCItemMgt, hServerItemRandom, varValue3);
-		printf("Read values -> saw: %6.2f | triangle: %6.2f | random: %6.2f\n", 
-			varValue1.fltVal, varValue2.fltVal, varValue3.fltVal);
-
-		Sleep(1000);
-	}
-	printf("\n");
-
-
-	printf("Removing the OPC itens...\n");
-	RemoveItem(pIOPCItemMgt, hServerItemSaw);
-	RemoveItem(pIOPCItemMgt, hServerItemTriangle);
-	RemoveItem(pIOPCItemMgt, hServerItemRandom);
-
-	printf("Removing the OPC group object...\n");
-    pIOPCItemMgt->Release();
-	RemoveGroup(pIOPCServer, hServerGroup);
-
-	printf("Removing the OPC server object...\n");
-	pIOPCServer->Release();
-
-	printf ("Releasing the COM environment...\n");
-	CoUninitialize();
-
-	return 0;
-}
-
-IOPCServer* InstantiateServer(wchar_t ServerName[])
-{
 	CLSID CLSID_OPCServer;
 	HRESULT hr;
 
 	// get the CLSID from the OPC Server Name:
-	hr = CLSIDFromString(ServerName, &CLSID_OPCServer);
+	hr = CLSIDFromString(OPC_SERVER_NAME, &CLSID_OPCServer);
 	_ASSERT(!FAILED(hr));
-
 
 	//queue of the class instances to create
 	LONG cmq = 1; // nbr of class instance to create.
-	MULTI_QI queue[1] =
-		{{&IID_IOPCServer,
-		NULL,
-		0}};
-
-	//Server info:
-	//COSERVERINFO CoServerInfo =
-    //{
-	//	/*dwReserved1*/ 0,
-	//	/*pwszName*/ REMOTE_SERVER_NAME,
-	//	/*COAUTHINFO*/  NULL,
-	//	/*dwReserved2*/ 0
-    //}; 
+	MULTI_QI queue[1] = {{&IID_IOPCServer, NULL, 0}};
 
 	// create an instance of the IOPCServer
-	hr = CoCreateInstanceEx(CLSID_OPCServer, NULL, CLSCTX_SERVER,
-		/*&CoServerInfo*/NULL, cmq, queue);
+	hr = CoCreateInstanceEx(CLSID_OPCServer, NULL, CLSCTX_SERVER, NULL, cmq, queue);
 	_ASSERT(!hr);
 
-	// return a pointer to the IOPCServer interface:
-	return(IOPCServer*) queue[0].pItf;
-}
+	pIOPCServer = (IOPCServer*) queue[0].pItf;
 
-void AddTheGroup(IOPCServer* pIOPCServer, IOPCItemMgt* &pIOPCItemMgt, 
-				 OPCHANDLE& hServerGroup)
-{
+	/* ---------------------------------------------------------------------------------- */
+	printf("Adding a group in the INACTIVE state for the moment...\n");
+	//AddTheGroup(pIOPCServer, pIOPCItemMgt, hServerGroup);
 	DWORD dwUpdateRate = 0;
 	OPCHANDLE hClientGroup = 0;
 
 	// Add an OPC group and get a pointer to the IUnknown I/F:
-    HRESULT hr = pIOPCServer->AddGroup(/*szName*/ L"Group1",
+    hr = pIOPCServer->AddGroup(
+		/*szName*/ L"Group1",
 		/*bActive*/ FALSE,
 		/*dwRequestedUpdateRate*/ 1000,
 		/*hClientGroup*/ hClientGroup,
@@ -143,38 +70,67 @@ void AddTheGroup(IOPCServer* pIOPCServer, IOPCItemMgt* &pIOPCItemMgt,
 		/*riid*/ IID_IOPCItemMgt,
 		/*ppUnk*/ (IUnknown**) &pIOPCItemMgt);
 	_ASSERT(!FAILED(hr));
-}
-	
-void AddTheItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE& hServerItem, wchar_t ITEM_ID[])
-{
-	HRESULT hr;
+
+	/* ---------------------------------------------------------------------------------- */
+	size_t m;
+	// Saw-toothed Waves
+	wcstombs_s(&m, buf, 100, SAW_ID, _TRUNCATE);
+	printf("Adding the item %s to the group...\n", buf);
+
+	// Triangle Waves
+	wcstombs_s(&m, buf, 100, TRIANGLE_ID, _TRUNCATE);
+	printf("Adding the item %s to the group...\n", buf);
+
+	// Random
+	wcstombs_s(&m, buf, 100, RANDOM_ID, _TRUNCATE);
+	printf("Adding the item %s to the group...\n", buf);
 
 	// Array of items to add:
-	OPCITEMDEF ItemArray[1] =
+	OPCITEMDEF ItemArray[3] =
 	{{
-	/*szAccessPath*/ L"",
-	/*szItemID*/ ITEM_ID,
-	/*bActive*/ TRUE,
-	/*hClient*/ 1,
-	/*dwBlobSize*/ 0,
-	/*pBlob*/ NULL,
-	/*vtRequestedDataType*/ VT,
-	/*wReserved*/0
+		/*szAccessPath*/ L"",
+		/*szItemID*/ SAW_ID,
+		/*bActive*/ TRUE,
+		/*hClient*/ 1,
+		/*dwBlobSize*/ 0,
+		/*pBlob*/ NULL,
+		/*vtRequestedDataType*/ VT,
+		/*wReserved*/0
+	},{
+		/*szAccessPath*/ L"",
+		/*szItemID*/ TRIANGLE_ID,
+		/*bActive*/ TRUE,
+		/*hClient*/ 1,
+		/*dwBlobSize*/ 0,
+		/*pBlob*/ NULL,
+		/*vtRequestedDataType*/ VT,
+		/*wReserved*/0
+	},{
+		/*szAccessPath*/ L"",
+		/*szItemID*/ RANDOM_ID,
+		/*bActive*/ TRUE,
+		/*hClient*/ 1,
+		/*dwBlobSize*/ 0,
+		/*pBlob*/ NULL,
+		/*vtRequestedDataType*/ VT,
+		/*wReserved*/0
 	}};
 
-	//Add Result:
+	// Add Result:
 	OPCITEMRESULT* pAddResult=NULL;
 	HRESULT* pErrors = NULL;
 
 	// Add an Item to the previous Group:
-	hr = pIOPCItemMgt->AddItems(1, ItemArray, &pAddResult, &pErrors);
+	hr = pIOPCItemMgt->AddItems(3, ItemArray, &pAddResult, &pErrors);
 	if (hr != S_OK){
 		printf("Failed call to AddItems function. Error code = %x\n", hr);
 		exit(0);
 	}
 
 	// Server handle for the added item:
-	hServerItem = pAddResult[0].hServer;
+	hServerItemSaw = pAddResult[0].hServer;
+	hServerItemTriangle = pAddResult[1].hServer;
+	hServerItemRandom = pAddResult[2].hServer;
 
 	// release memory allocated by the server:
 	CoTaskMemFree(pAddResult->pBlob);
@@ -184,61 +140,81 @@ void AddTheItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE& hServerItem, wchar_t ITEM_
 
 	CoTaskMemFree(pErrors);
 	pErrors = NULL;
-}
 
-void ReadItem(IUnknown* pGroupIUnknown, OPCHANDLE hServerItem, VARIANT& varValue)
-{
-	// value of the item:
-	OPCITEMSTATE* pValue = NULL;
+	/* ---------------------------------------------------------------------------------- */
 
-	//get a pointer to the IOPCSyncIOInterface:
-	IOPCSyncIO* pIOPCSyncIO;
-	pGroupIUnknown->QueryInterface(__uuidof(pIOPCSyncIO), (void**) &pIOPCSyncIO);
+	VARIANT varValue1, varValue2, varValue3; 
+	VariantInit(&varValue1);
+	VariantInit(&varValue2);
+	VariantInit(&varValue3);
 
-	// read the item value from the device:
-	HRESULT* pErrors = NULL; //to store error code(s)
-	HRESULT hr = pIOPCSyncIO->Read(OPC_DS_DEVICE, 1, &hServerItem, &pValue, &pErrors);
-	_ASSERT(!hr);
-	_ASSERT(pValue!=NULL);
+	printf ("Reading synchronously during 10 seconds...\n\n");
+	OPCHANDLE hServerItems[] = { hServerItemSaw, hServerItemTriangle, hServerItemRandom };
+	for (i=0; i<10; i++) {
+		//ReadItem(pIOPCItemMgt, hServerItemSaw, varValue1);
+		// value of the item:
+		OPCITEMSTATE* pValue = NULL;
+		IOPCSyncIO* pIOPCSyncIO;
+		HRESULT* pErrors = NULL; //to store error code(s)
 
-	varValue = pValue[0].vDataValue;
+		//get a pointer to the IOPCSyncIOInterface:
+		pIOPCItemMgt->QueryInterface(__uuidof(pIOPCSyncIO), (void**) &pIOPCSyncIO);
 
-	//Release memeory allocated by the OPC server:
-	CoTaskMemFree(pErrors);
-	pErrors = NULL;
+		// read the item value from the device:
+		hr = pIOPCSyncIO->Read(OPC_DS_DEVICE, 3, hServerItems, &pValue, &pErrors);
+		_ASSERT(!hr);
+		_ASSERT(pValue!=NULL);
 
-	CoTaskMemFree(pValue);
-	pValue = NULL;
+		varValue1 = pValue[0].vDataValue;
+		varValue2 = pValue[1].vDataValue;
+		varValue3 = pValue[2].vDataValue;
 
-	// release the reference to the IOPCSyncIO interface:
-	pIOPCSyncIO->Release();
-}
+		//Release memeory allocated by the OPC server:
+		CoTaskMemFree(pErrors);
+		pErrors = NULL;
 
-void RemoveItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE hServerItem)
-{
-	// server handle of items to remove:
-	OPCHANDLE hServerArray[1];
-	hServerArray[0] = hServerItem;
-	
-	//Remove the item:
-	HRESULT* pErrors; // to store error code(s)
-	HRESULT hr = pIOPCItemMgt->RemoveItems(1, hServerArray, &pErrors);
+		CoTaskMemFree(pValue);
+		pValue = NULL;
+
+		// release the reference to the IOPCSyncIO interface:
+		pIOPCSyncIO->Release();
+
+		printf("Read values -> saw: %6.2f | triangle: %6.2f | random: %6.2f\n", 
+			varValue1.fltVal, varValue2.fltVal, varValue3.fltVal);
+
+		Sleep(1000);
+	}
+	printf("\n");
+
+	/* ---------------------------------------------------------------------------------- */
+	printf("Removing the OPC itens...\n");
+	hr = pIOPCItemMgt->RemoveItems(3, hServerItems, &pErrors);
 	_ASSERT(!hr);
 
 	//release memory allocated by the server:
 	CoTaskMemFree(pErrors);
 	pErrors = NULL;
-}
 
-void RemoveGroup (IOPCServer* pIOPCServer, OPCHANDLE hServerGroup)
-{
-	// Remove the group:
-	HRESULT hr = pIOPCServer->RemoveGroup(hServerGroup, FALSE);
+	/* ---------------------------------------------------------------------------------- */
+	printf("Removing the OPC group object...\n");
+    pIOPCItemMgt->Release();
+
+	/* ---------------------------------------------------------------------------------- */
+	printf("Removing the OPC server object...\n");
+	hr = pIOPCServer->RemoveGroup(hServerGroup, FALSE);
 	if (hr != S_OK){
 		if (hr == OPC_S_INUSE)
 			printf ("Failed to remove OPC group: object still has references to it.\n");
 		else printf ("Failed to remove OPC group. Error code = %x\n", hr);
 		exit(0);
 	}
+	pIOPCServer->Release();
+
+	/* ---------------------------------------------------------------------------------- */
+	printf ("Releasing the COM environment...\n");
+	CoUninitialize();
+
+	return 0;
 }
+
 
--- OPC_estudos/Main.h
+++ OPC_estudos/Main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-IOPCServer *InstantiateServer(wchar_t ServerName[]);
-void AddTheGroup(IOPCServer* pIOPCServer, IOPCItemMgt* &pIOPCItemMgt, 
-				 OPCHANDLE& hServerGroup);
-void AddTheItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE& hServerItem, wchar_t ITEM_ID[]);
-void ReadItem(IUnknown* pGroupIUnknown, OPCHANDLE hServerItem, VARIANT& varValue);
-void RemoveItem(IOPCItemMgt* pIOPCItemMgt, OPCHANDLE hServerItem);
-void RemoveGroup(IOPCServer* pIOPCServer, OPCHANDLE hServerGroup);
 
 #endif 
 
